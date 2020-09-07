5b6fb1619afb19b6b47d59afa0a2a66d794625c8
Daniel Menezes <danielmenezes42@gmail.com>
2020-09-07 18:43:22 -0300

    implemented matrix mult. naive algorithm

--- matrix_lib.c
+++ matrix_lib.c
@@ -21,8 +21,20 @@ int scalar_matrix_mult(float scalar_value, struct matrix *matrix){
  *Essa função recebe 3 matrizes como argumentos de entrada e calcula o valor do produto da
  *matriz A pela matriz B. O resultado da operação deve ser retornado na matriz C. Em caso
  *de sucesso, a função deve retornar o valor 1. Em caso de erro, a função deve retornar 0.
+ *
+ * NAIVE IMPLEMENTATION
  */
 int matrix_matrix_mult(struct matrix *matrixA, struct matrix * matrixB, struct matrix * matrixC) {
-    /* TODO */
-    return 0;
+    int i,j,k;
+    if (matrixA->width != matrixB->height || matrixA->height != matrixC->height || matrixB->width != matrixC->width)
+        return 0;
+    for (i=0; i< matrixC->height ; i++) {
+        for (j=0; j<matrixC->width; j++) {
+            for (k=0; k<matrixA->width; k++){
+                matrixC->rows[i*matrixC->width+j] += matrixA->rows[i*matrixA->width + k] *
+                                                     matrixB->rows[k*matrixB->width + j];
+            }
+        }
+    }
+    return 1;
 }
