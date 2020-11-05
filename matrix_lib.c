60aa64a03a8f464b12e89202dbdb06fd92bef00e
Daniel Menezes <danielmenezes42@gmail.com>
2020-11-05 16:51:49 -0300

    scalar multi w/ threads

--- matrix_lib.c
+++ matrix_lib.c
@@ -5,6 +5,26 @@
 
 static int n_threads = 1;
 
+struct scalar_mult_arg {
+    float *base;
+    int elem_qty;
+    int scalar_value;
+};
+
+static void *scalar_mult_worker(void *a) {
+    int i;
+    struct scalar_mult_arg *arg = (struct scalar_mult_arg *) a;
+    __m256 temp;
+    const __m256 scalar_vec = _mm256_set1_ps(arg->scalar_value);
+    for (i=0; i < arg->elem_qty; i+=8) {
+        temp = _mm256_load_ps(arg->base+i);
+        temp = _mm256_mul_ps(scalar_vec, temp);
+        _mm256_store_ps(arg->base+i, temp);
+    }
+    pthread_exit(NULL);
+}
+
+
 /*
  *Essa função recebe um valor escalar e uma matriz como argumentos de entrada e calcula o
  *produto do valor escalar pela matriz. O resultado da operação deve ser retornado na matriz
@@ -12,15 +32,26 @@ static int n_threads = 1;
  *função deve retornar 0.
  */
 int scalar_matrix_mult(float scalar_value, struct matrix *matrix){
-    int i;
-    __m256 temp;
-    const __m256 scalar_vec = _mm256_set1_ps(scalar_value);
+    int i, line_qty;
+    pthread_t *threads;
+    struct scalar_mult_arg *args;
     if (matrix && matrix->rows){
-        for (i=0; i < matrix->width*matrix->height; i+=8) {
-            temp = _mm256_load_ps(matrix->rows+i);
-            temp = _mm256_mul_ps(scalar_vec, temp);
-            _mm256_store_ps(matrix->rows+i, temp);
+        threads = malloc(sizeof(pthread_t) * n_threads);
+        args = malloc(sizeof(struct scalar_mult_arg) * n_threads);
+        line_qty = matrix->height/n_threads;
+        for (i=0; i<n_threads; i++) {
+            args[i].base = matrix->rows+i*line_qty;
+            args[i].elem_qty = matrix->width*line_qty;
+            args[i].scalar_value = scalar_value;
+            pthread_create(&threads[i],NULL, scalar_mult_worker, &args[i]);
         }
+
+        for (i=0; i<n_threads; i++) {
+            if (pthread_join(threads[i], NULL))
+                return 0;
+        }
+        free(args);
+        free(threads);
         return 1;
     }
     return 0;
